1f6683ffafec81721fb8be1dbac8a158dd05dc92
anthony cantor <ar.cantor@gmail.com>
2013-12-15 02:04:47 -0700

    added button state procedures
    doesnt block to wait for switch bouncing to calm, but tracks
    the state of the button using timing. this will allow ui elements
    to detect when the switch is being held down and the encoder is
    being rotated at the same time.

--- src/ui.c
+++ src/ui.c
@@ -11,26 +11,89 @@ static void selector_set(uint32_t val, uint32_t range) {
   ADP_selector_set(range*1000UL + val);
 }
 
-static int button_wait(uint8_t state, uint32_t millisecs, uint32_t max) {
-  uint32_t start, t, last;
+typedef enum {
+  UI_BTN_STATE_OFF = 0,
+  UI_BTN_STATE_TRIGGER,
+  UI_BTN_STATE_HOLDING_A,
+  UI_BTN_STATE_HOLDING_B,
+  UI_BTN_STATE_RELEASED
+} ui_btn_state;
 
-  start = ADP_ts_millis();
-  last = start;
-  while( (t = ADP_ts_millis()) < (start + max) ) {
-    if(ADP_button_check() != state)
-      last = t;
-    else if(t - last >= millisecs)
-      return 0;
+static struct {
+  uint32_t t;
+  uint16_t triggers;
+  ui_btn_state state;
+} g_btn;
+
+
+static void btn_state_init() {
+  g_btn.state = UI_BTN_STATE_OFF;
+  g_btn.t = ADP_ts_millis();
+  g_btn.triggers = 0;
+}
+
+#define UI_BTN_EDGE_THRESH 30 /* milliseconds */
+
+static inline int btn_state_holding() {
+  return (g_btn.state == UI_BTN_STATE_HOLDING_A
+          || g_btn.state == UI_BTN_STATE_HOLDING_B);
+}
+
+static inline int btn_state_released() {
+  return (g_btn.state == UI_BTN_STATE_RELEASED);
+}
+
+static void btn_state_update() {
+  uint8_t val;
+
+  if(btn_state_released())
+    return;
+
+  val = ADP_button_check();
+
+  switch(g_btn.state) {
+  case UI_BTN_STATE_OFF:
+    if(val != 0) {
+      g_btn.t = ADP_ts_millis();
+      g_btn.state = UI_BTN_STATE_TRIGGER;
+    }
+    break;
+  case UI_BTN_STATE_TRIGGER:
+    if(val == 0)
+      g_btn.state = UI_BTN_STATE_HOLDING_B;
+    else
+      if(ADP_ts_millis() - g_btn.t >= UI_BTN_EDGE_THRESH)
+        g_btn.state = UI_BTN_STATE_HOLDING_A;
+    break;
+  case UI_BTN_STATE_HOLDING_A:
+    if(val == 0) {
+      g_btn.state = UI_BTN_STATE_HOLDING_B;
+      g_btn.t = ADP_ts_millis();
+    }
+    break;
+  case UI_BTN_STATE_HOLDING_B:
+    if(val != 0) {
+      if(g_btn.t < UI_BTN_EDGE_THRESH)
+        g_btn.state = UI_BTN_STATE_HOLDING_A;
+      else {
+        g_btn.state = UI_BTN_STATE_TRIGGER;
+        g_btn.t = ADP_ts_millis();
+      }
+    }
+    else if(ADP_ts_millis() - g_btn.t >= 100) {
+      g_btn.state = UI_BTN_STATE_RELEASED;
+    }
+    break;
+  default:
+    ;/* stick in this state until manual reset */
   }
-
-  return 1;
 }
 
 size_t ui_input(uint16_t col, uint16_t row, /* starting col,row of interaction */
                 uint8_t *data, size_t len,  /* buffer to write user input into */
                 uint8_t opts) {             /* options, see enum */
   size_t i, j;
-  uint8_t pos0, pos1, btn, new_char, ok, terminal_ch;
+  uint8_t pos0, pos1, new_char, ok, terminal_ch;
   uint16_t col_space, prev_amt, cols, rows;
 
   display_dims(&cols, &rows);
@@ -42,12 +105,12 @@ size_t ui_input(uint16_t col, uint16_t row, /* starting col,row of interaction *
   terminal_ch = ADP_display_input_terminal_char();
   pos0 = 0;
   selector_set(0x61 - INPUT_CHAR_OFFSET, INPUT_CHAR_RANGE);
-  btn = 0;
+  btn_state_init();
 
   for(i = 0; i < len; i++) {
     new_char = 1;
     while(1) {
-      btn = ADP_button_check();
+      btn_state_update();
       pos1 = INPUT_CHAR_OFFSET + (ADP_selector_position() % INPUT_CHAR_RANGE);
 
       if(pos1 != pos0 || new_char) {
@@ -67,8 +130,8 @@ size_t ui_input(uint16_t col, uint16_t row, /* starting col,row of interaction *
         ADP_display_write((pos0 == terminal_ch)? ok : pos0);
       }
 
-      if(btn != 0) {
-        button_wait(0, 200, 1000);
+      if(btn_state_released()) {
+        btn_state_init();
         if(pos0 == terminal_ch)
           goto done;
         data[i] = pos0;
