cd09171b00eff09aad6b768e7d6cfa5d43898f81
anthony cantor <ar.cantor@gmail.com>
2013-12-16 23:02:12 -0700

    factored out pieces of ui_input to simplify

--- src/ui.c
+++ src/ui.c
@@ -19,21 +19,15 @@ typedef enum {
   UI_BTN_STATE_RELEASED
 } ui_btn_state;
 
-typedef enum {
-  UI_BTN_FLAGS_HELD = 1
-} ui_btn_flags;
-
 static struct {
   uint32_t t;
   ui_btn_state state;
-  uint8_t flags;
 } g_btn;
 
 
 static void btn_state_init() {
   g_btn.state = UI_BTN_STATE_OFF;
   g_btn.t = ADP_ts_millis();
-  g_btn.flags = 0;
 }
 
 #define UI_BTN_EDGE_THRESH 350 /* milliseconds */
@@ -46,10 +40,6 @@ static inline int btn_state_down() {
 
 static inline int btn_state_released() {
   return (g_btn.state == UI_BTN_STATE_RELEASED);
-}
-
-static inline uint8_t btn_flag_held() {
-  return (g_btn.flags & UI_BTN_FLAGS_HELD);
 }
 
 static inline void btn_state_update() {
@@ -75,7 +65,6 @@ static inline void btn_state_update() {
         g_btn.state = UI_BTN_STATE_HOLDING_A;
     break;
   case UI_BTN_STATE_HOLDING_A:
-    g_btn.flags |= UI_BTN_FLAGS_HELD;
     if(val == 0) {
       g_btn.state = UI_BTN_STATE_HOLDING_B;
       g_btn.t = ADP_ts_millis();
@@ -122,96 +111,142 @@ static inline uint16_t print_input(uint8_t *data, uint8_t len,
   return prev_amt;
 }
 
+struct ui_input_params {
+  uint16_t col_space, col, row;
+  uint8_t opts;
+};
+
+struct ui_input_state {
+  uint32_t delete_amt;
+  uint8_t *data;
+  size_t i;
+  uint8_t delete_state, pos, refresh;
+};
+
 #define UI_INPUT_DELETE_START_POS 1048576UL
+
+static void ui_input_down(struct ui_input_state *state,
+                          const struct ui_input_params *params) {
+  uint32_t amt;
+  uint16_t j;
+
+  if((state->delete_state & 1) == 0) {
+    state->delete_state = 1;
+    ADP_selector_set(UI_INPUT_DELETE_START_POS);
+    state->delete_amt = 0;
+  }
+
+  amt = UI_INPUT_DELETE_START_POS - ADP_selector_position();
+  if(amt < UI_INPUT_DELETE_START_POS) {
+    if(amt > state->i) {
+      amt = state->i;
+      ADP_selector_set(UI_INPUT_DELETE_START_POS - amt);
+    }
+    if(amt != state->delete_amt) {
+      state->delete_amt = amt;
+      ADP_display_cursor_set(params->col, params->row);
+      j = print_input(state->data, state->i-state->delete_amt,
+                        params->col_space-1, params->opts);
+      for(; j < params->col_space; j++)
+        ADP_display_write(' ');
+      if(state->delete_amt != 0)
+        state->delete_state |= 2;
+    }
+  }
+  else {
+    state->delete_state |= 2;
+    ADP_selector_set(UI_INPUT_DELETE_START_POS);
+  }
+}
+
+static uint8_t ui_input_not_down(struct ui_input_state *state,
+                                 const struct ui_input_params *params) {
+  uint8_t pos, terminal_ch, ok, ret;
+
+  ret = -1;
+  ok = ADP_display_ok_char();
+  terminal_ch = ADP_display_input_terminal_char();
+
+  if(state->delete_state & 1) {
+    state->delete_state ^= 1;
+    selector_set(state->pos - INPUT_CHAR_OFFSET, INPUT_CHAR_RANGE);
+  }
+
+  if((state->delete_state & 2) && btn_state_released()) {
+    state->i -= state->delete_amt;
+    state->refresh = 1;
+    btn_state_init();
+  }
+  else {
+    pos = INPUT_CHAR_OFFSET + (ADP_selector_position() % INPUT_CHAR_RANGE);
+
+    if(pos != state->pos || state->refresh) {
+      state->pos = pos;
+      ADP_display_cursor_set(params->col, params->row);
+      print_input(state->data, state->i, params->col_space-1, params->opts);
+      ADP_display_write((state->pos == terminal_ch)? ok : state->pos);
+      state->refresh = 0;
+    }
+
+    if(btn_state_released()) {
+      if(state->pos == terminal_ch)
+        return 0;
+      state->data[state->i] = state->pos;
+      ret = 1;
+      btn_state_init();
+    }
+  }
+
+  return ret;
+}
 
 size_t ui_input(uint16_t col, uint16_t row, /* starting col,row of interaction */
                 uint8_t *data, size_t len,  /* buffer to write user input into */
                 uint8_t opts) {             /* options, see enum */
-  size_t i;
-  uint8_t pos, tmp_pos, refresh, terminal_ch, ok, delete_state, brk;
-  uint16_t col_space, cols, rows, tmp, j;
-  uint32_t prev_delete_amt, delete_amt;
+  uint16_t cols, rows;
+  uint8_t brk;
+  struct ui_input_params params;
+  struct ui_input_state state;
 
   display_dims(&cols, &rows);
-  col_space = cols - col;
-  if(col_space < 2)
+  params.col_space = cols - col;
+  if(params.col_space < 2)
     return 0;
+  params.col = col;
+  params.row = row;
+  params.opts = opts;
 
-  delete_amt = 0;
-  prev_delete_amt = 0;
-  delete_state = 0;
-  ok = ADP_display_ok_char();
-  terminal_ch = ADP_display_input_terminal_char();
-  pos = 0;
+  state.delete_amt = 0;
+  state.delete_state = 0;
+  state.data = data;
+  state.pos = 0;
+
   selector_set(0x61 - INPUT_CHAR_OFFSET, INPUT_CHAR_RANGE);
   btn_state_init();
 
-  for(i = 0; i < len; i++) {
-    refresh = 1;
+  for(state.i = 0; state.i < len; state.i++) {
+    state.refresh = 1;
     brk = 0;
     while(brk == 0) {
       btn_state_update();
 
       if(btn_state_down()) {
-        if( (delete_state & 1) == 0) {
-          delete_state = 1;
-          ADP_selector_set(UI_INPUT_DELETE_START_POS);
-          prev_delete_amt = 0; /* arbitrary non-zero */
-        }
-        delete_amt = UI_INPUT_DELETE_START_POS - ADP_selector_position();
-        if(delete_amt < UI_INPUT_DELETE_START_POS) {
-          if(delete_amt > i) {
-            delete_amt = i;
-            ADP_selector_set(UI_INPUT_DELETE_START_POS - delete_amt);
-          }
-          if(delete_amt != prev_delete_amt) {
-            prev_delete_amt = delete_amt;
-            ADP_display_cursor_set(col, row);
-            tmp = print_input(data, i-delete_amt, col_space-1, opts);
-            for(j = tmp; j < col_space; j++)
-              ADP_display_write(' ');
-            if(delete_amt != 0)
-              delete_state |= 2;
-          }
-        }
-        else {
-          delete_state |= 2;
-          ADP_selector_set(UI_INPUT_DELETE_START_POS);
-        }
+        ui_input_down(&state, &params);
       }
       else {
-        if(delete_state & 1) {
-          delete_state ^= 1;
-          selector_set(pos - INPUT_CHAR_OFFSET, INPUT_CHAR_RANGE);
-        }
-        if((delete_state & 2) && btn_state_released()) {
-          i -= delete_amt;
-          refresh = 1;
-          btn_state_init();
-        }
-        else {
-          tmp_pos = INPUT_CHAR_OFFSET + (ADP_selector_position() % INPUT_CHAR_RANGE);
-
-          if(tmp_pos != pos || refresh) {
-            pos = tmp_pos;
-            ADP_display_cursor_set(col, row);
-            print_input(data, i, col_space-1, opts);
-            ADP_display_write((pos == terminal_ch)? ok : pos);
-            refresh = 0;
-          }
-
-          if(btn_state_released()) {
-            if(pos == terminal_ch)
-              goto done;
-            data[i] = pos;
-            brk = 1;
-            btn_state_init();
-          }
+        switch(ui_input_not_down(&state, &params)) {
+        case 0:
+          goto done;
+        case 1:
+          brk = 1;
+          break;
+        default:
+          ; /* nothing */
         }
       }
     }
   }
 
 done:
-  return i;
+  return state.i;
 }
