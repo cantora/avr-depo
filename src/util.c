4d159de0f1e6607530fb6dda3f96169ee711b6a0
anthony cantor <ar.cantor@gmail.com>
2013-12-19 02:12:43 -0700

    only use radix we know we have enough room for in buf

--- src/util.c
+++ src/util.c
@@ -4,6 +4,11 @@ void debug_print_n(uint32_t n, int radix) {
   char buf[16];
   char *result;
 
+  /* buf[16] will hold decimal and hex 32 bit int
+   * but wont hold a binary conversion */
+  if(radix != 10 && radix != 16)
+    return; 
+
   result = ultoa(n, buf, radix);
   if(result) {
     ADP_debug_print(result);
