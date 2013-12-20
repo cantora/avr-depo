e0446d8d065c382e80b28116e70c275a1137b9e8
anthony cantor <ar.cantor@gmail.com>
2013-12-20 01:17:45 -0700

    add clear_cols function to display module

--- src/display.c
+++ src/display.c
@@ -4,6 +4,15 @@ inline void display_dims(uint16_t *cols, uint16_t *rows) {
   ADP_display_dims(cols, rows);
 }
 
+inline void display_clear_cols(uint16_t col, uint16_t row) {
+  uint16_t cols, rows, i;
+
+  display_dims(&cols, &rows);
+  ADP_display_cursor_set(col, row);
+  for(i = col; i < cols; i++)
+    ADP_display_write(' ');
+}
+
 void display_print(uint16_t col, uint16_t row, const char *s) {
   int i;
   uint16_t cols, rows;
--- src/display.h
+++ src/display.h
@@ -2,6 +2,7 @@
 #define AVR_DEPO_display_h
 
 void display_dims(uint16_t *cols, uint16_t *rows);
+void display_clear_cols(uint16_t cols, uint16_t row);
 void display_print(uint16_t col, uint16_t row, const char *s);
 void display_print_n(uint16_t col, uint16_t row, uint32_t n, int radix);
 int display_print_chunk(uint16_t col, uint16_t row, const char *s);
